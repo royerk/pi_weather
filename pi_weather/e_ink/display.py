6d6d0a0306c16b75c2303485d00e853760bb4e67
Lysk <kroyer.github@gmail.com>
2024-03-06 11:54:15 -0800

    rotate image

--- pi_weather/e_ink/display.py
+++ pi_weather/e_ink/display.py
@@ -45,7 +45,7 @@ for i, device_name in enumerate(data):
     text = f"{device_alias}: {data[device_name]['temperature']:.1f} C ({delta_minutes}m ago)"
     draw.text((x, y + i * y_delta), text, font=font20, fill=0)
 
-image.rotate(180)
+image = image.rotate(180)
 
 epd.display(epd.getbuffer(image))
 epd.sleep()
